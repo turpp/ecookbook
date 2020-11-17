4bafda7d10cc65993d01c7b2ed3c0bdeab6a12ce
turpp <white.tripp@icloud.com>
2020-11-17 06:39:33 -0600

    created the new form for recipe. added textarea for better typing experence

--- ecookbook/app/controllers/recipes_controller.rb
+++ ecookbook/app/controllers/recipes_controller.rb
@@ -5,10 +5,21 @@ class RecipesController < ApplicationController
         erb :"recipes/index"
     end
 
+
+    get '/recipes/new' do
+        erb :'recipes/new'
+    end
+
+    post '/recipes' do
+        binding.pry
+        "We made it"
+    end
+
     get '/recipes/:id' do
         @recipe=Recipe.find_by(id: params[:id])
         erb :"recipes/show"
     end 
+
 
 
 
