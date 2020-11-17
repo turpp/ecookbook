e423c4f94f5bc67f535e44ed9b87a9f1215c231b
turpp <white.tripp@icloud.com>
2020-11-17 09:52:17 -0600

    added buttons to navagate around the views. added back to account button to show and edit

--- ecookbook/app/controllers/users_controller.rb
+++ ecookbook/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        binding.pry
+        
         @user=User.find_by(params[:user])
         if @user
             session[:user_id]=@user.id
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
     end
 
     get '/user/account' do
-        binding.pry
+        
+        @user=User.find_by(id: session[:user_id])
         erb :'users/account'
     end
 
